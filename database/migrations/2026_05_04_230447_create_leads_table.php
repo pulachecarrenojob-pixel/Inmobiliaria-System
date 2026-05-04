c2231bb1bc149adfba5ea6aa1eded83dc09898c5
pulachecarrenojob <pulachecarrenojob@gmail.com>
2026-05-04 18:12:36 -0500

    execute migration

--- database/migrations/2026_05_04_230447_create_leads_table.php
+++ database/migrations/2026_05_04_230447_create_leads_table.php
@@ -4,24 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('email');
+            $table->string('telefono');
+            $table->string('interes'); // casa, terreno, depa
+            $table->string('estado')->default('nuevo');
             $table->timestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('leads');
     }
 };
